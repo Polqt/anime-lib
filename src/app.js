7f17915e10947e50f7e94b6ffae8367a36af47cf
Pol Hidalgo <80904366+Polqt@users.noreply.github.com>
2024-12-29 20:29:27 +0800

    feat: add file upload middleawre with Multer and Cloudinary

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import healthCheckRouter from './routes/healthcheck.route.js'
+import cookieParser from 'cookie-parser'
 
 const app = express()
 
@@ -14,6 +15,7 @@ app.use(cors({
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ extended: true, limit: '50mb' }))
 app.use(express.static('public'))
+app.use(cookieParser())
 
 // Routes
 app.use('/api/v1/healthcheck', healthCheckRouter)
